0557f3f34ca1675a3b81354b87b3f7fffa813882
so-masuda <so.masuda.2003@gmail.com>
2026-01-25 22:07:42 +0900

    Initial Commit

---
+++ GratefulMoments/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GratefulMoments
+//
+//  Created by 増田創 on 2026/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GratefulMoments/GratefulMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GratefulMomentsApp.swift
+//  GratefulMoments
+//
+//  Created by 増田創 on 2026/01/25.
+//
+
+import SwiftUI
+
+@main
+struct GratefulMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
