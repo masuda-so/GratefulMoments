a94b29125cc0ad7ff22df20b68bb3ab82e3bea12
masudaso <so.masuda.2003@pm.me>
2026-04-28 19:00:11 +0900

    Initial Commit

---
+++ GratefulMoments/GratefulMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GratefulMomentsApp.swift
+//  GratefulMoments
+//
+//  Created by 増田創 on 2026/04/17.
+//
+
+import SwiftUI
+
+@main
+struct GratefulMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
